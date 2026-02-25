8e1d26d39080cd04759268a37f27843c9e7e7da9
henrycyb24-coder <henrycyb24@gmail.com>
2026-02-25 02:59:32 +0000

    Update server.js

--- server.js
+++ server.js
@@ -97,7 +97,6 @@ app.get('/download', (req, res) => {
 START SERVER
 ====================================
 */
-const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
